36f7ded769de81bd4550f81bb6c8280ec5c5f4ff
Benjamin Herrera Navarro <bherreranavarro@winsystems.com>
2024-03-07 17:37:18 +0000

    Open shared library from python

--- stress_test.py
+++ stress_test.py
@@ -922,7 +922,7 @@ def handle_additional_input(input_win, key):
     pass
 
 def run_curses(stdscr):
-    libc = CDLL("libc.so.6")
+    libc = CDLL("Loopback_EMUC2/loopback.so")
     # stdscr.curs_set(0)
 
     log.info("SYSTEM-TEST: CAN: Setting up devices")
